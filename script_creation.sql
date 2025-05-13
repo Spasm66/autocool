93407174de50cf1e86027091d0ef7122cf5a3465
Spasm66 <loris.pfeiffer27@gmail.com>
2025-05-13 18:08:04 +0200

    update script creation sql

--- script_creation.sql
+++ script_creation.sql
@@ -14,9 +14,17 @@ CREATE TABLE abonne(
     PaimentAdhesion BOOLEAN,
     PaimentCaution BOOLEAN,
     RIBfourni VARCHAR (50)
-    CONSTRAINT "PK_NumAbonne" PRIMARY KEY (NumAbonne));
+    CONSTRAINT "PK_NumAbonne" PRIMARY KEY (NumAbonne)
+    );
 
 CREAT formule(
     CodeFormule SERIAL,
-    
-)
+    LibelleFormule VARCHAR (15),
+    FraisAdhesion INT,
+    TarifMensuel INT,
+    PartSorcial INT,
+    DepotGarantie INT,
+    Caution INT,
+    CONSTRAINT "pk_CodeFormule" PRIMARY KEY (CodeFormule)
+    );
+

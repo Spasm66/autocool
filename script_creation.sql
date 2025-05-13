841df18b07ea3962aae1b39a09d7e630a00176b2
Spasm66 <loris.pfeiffer27@gmail.com>
2025-05-13 18:03:51 +0200

    script creation sql

---
+++ script_creation.sql
@@ -0,0 +1,22 @@
+CREATE TABLE abonne(
+    NumAbonne SERIAL,
+    Nom  VARCHAR (15),
+    Prenom VARCHAR (15),
+    DateNaissance DATE,
+    Rue VARCHAR (30),
+    CodePostal VARCHAR (5),
+    Tel CHAR (10),
+    TelMobile CHAR (10),
+    Email VARCHAR (30),
+    NumPermis VARCHAR (30),
+    LieuPermis VARCHAR (30),
+    DAtePermis DATE,
+    PaimentAdhesion BOOLEAN,
+    PaimentCaution BOOLEAN,
+    RIBfourni VARCHAR (50)
+    CONSTRAINT "PK_NumAbonne" PRIMARY KEY (NumAbonne));
+
+CREAT formule(
+    CodeFormule SERIAL,
+    
+)

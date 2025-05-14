5df2e4ea422ef6faa61942a0abf64974103a9fae
Nicolas Andrieu <niandrieu@emi.u-bordeaux.fr>
2025-05-14 18:06:06 +0200

    debut du menu et correction fonc 1

--- main_autocool.py
+++ main_autocool.py
@@ -9,49 +9,25 @@ with psycopg.connect("host="+HOST+" user="+USERNAME+" password="+PASS) as conn:
     print('Connecté à la base de données')
     #préparation de l'exécution des requêtes (à ne faire qu'une fois)
     with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
-        creation_table = "script_creation.sql"
-        """
-        try :
-            # Lecture du fichier SQL
-            with open(creation_table, 'r') as f:
-                sql_commands = f.read()        
-            cur.execute(sql_commands)
-            #conn.commit()
-            print("Fichier creation_table exécuté avec succès.")
-        except Exception as e:
-            print("Une erreur est survenue (si du au fait que les table existe deja pas de probleme): "+ str(e))
         
-        # test si deja des donnes
-        test_abonne = 'SELECT * FROM abonne;'
-        
-        try:
-            cur.execute(test_abonne)
-            print("Fichier SQL exécuté avec succès.")
-        except psycopg.Error as e:
-            exit("error when running: " + test_abonne + " : " + str(e))
-        if cur.rowcount == 0:
-            #charger les donnés
-            script_insertion = "script_insertion.sql"
-            try :
-                # Lecture du fichier SQL
-                with open(script_insertion, 'r') as f:
-                    sql_commands = f.read()        
-                cur.execute(sql_commands)
-                conn.commit()
-                print("Fichier script_insertion exécuté avec succès.")
-            except Exception as e:
-                exit("Une erreur est survenue : {e}")
-        """
         def lst_aderent(formule):
             # formule = (« Classique » ou « Coopérative « ou « Liberté »)
-            commande = 'select NumAbonne from  formule ,adhere  where LibelleFormule =(%(id)s) and  formule.CodeFormule=adhere.CodeFormule '
+            commande = 'select abonne.NumAbonne, Nom, Prenom from abonne, formule ,adhere  ' \
+            'where LibelleFormule =(%(id)s) ' \
+            'and abonne.NumAbonne = adhere.NumAbonne ' \
+            'and  formule.CodeFormule=adhere.CodeFormule '
             try:
                 cur.execute(commande,{'id':formule})
                 print("commande SQL exécuté avec succès.")
             except Exception as e:
                 exit("error when running: " + commande + " : " + str(e))
+        
+        
         lst_aderent("Classique")
         print(cur.fetchall())
+
+
+
         
         def Affi_adherent(num_abo):
             #Affichage d’un adhérent en fonction de son numero d'aderent
@@ -64,28 +40,28 @@ with psycopg.connect("host="+HOST+" user="+USERNAME+" password="+PASS) as conn:
         Affi_adherent(1)
         print(cur.fetchall())
         def Ajout_adherent(
-    Nom  ,
-    Prenom ,
-    DateNaissance ,
-    Rue ,
-    CodePostal ,
-    Tel ,
-    TelMobile ,
-    Email ,
-    NumPermis ,
-    LieuPermis ,
-    DatePermis ,
-    PaimentAdhesion ,
-    PaimentCaution ,
-    RIBfourni,
-    numformule ):
+                Nom  ,
+                Prenom ,
+                DateNaissance ,
+                Rue ,
+                CodePostal ,
+                Tel ,
+                TelMobile ,
+                Email ,
+                NumPermis ,
+                LieuPermis ,
+                DatePermis ,
+                PaimentAdhesion ,
+                PaimentCaution ,
+                RIBfourni,
+                numformule ):
             commande_abonne = 'INSERT INTO abonne (Nom, Prenom, DateNaissance, Rue, CodePostal, Tel, TelMobile, Email, NumPermis, LieuPermis, DatePermis, PaimentAdhesion, PaimentCaution, RIBfourni)'\
-        'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
            
             try:
                 cur.execute(commande_abonne, (
             Nom, Prenom, DateNaissance, Rue, CodePostal, Tel, TelMobile, Email, NumPermis, LieuPermis, DatePermis, PaimentAdhesion, PaimentCaution, RIBfourni
-        ))
+            ))
                 print("commande SQL exécuté avec succès.")
             except Exception as e:
                 exit("error when running: " + commande_abonne + " : " + str(e))
@@ -95,34 +71,86 @@ with psycopg.connect("host="+HOST+" user="+USERNAME+" password="+PASS) as conn:
             num_abonne = num_abonne_temp['max']
             print(num_abonne)
             commande_adhere = """
-        INSERT INTO adhere (NumAbonne, CodeFormule)
-        VALUES (%s, %s);
-        """
+            INSERT INTO adhere (NumAbonne, CodeFormule)
+            VALUES (%s, %s);
+            """
             try:
                 cur.execute(commande_adhere, (
             num_abonne,numformule
-        ))
+            ))
                 print("commande SQL exécuté avec succès.")
             except Exception as e:
                 exit("error when running: " + commande_adhere + " : " + str(e))
 
 
         Ajout_adherent(
-    Nom="Dupont",
-    Prenom="Jean",
-    DateNaissance="1985-05-15",
-    Rue="123 Rue de Paris",
-    CodePostal="75001",
-    Tel="0123456789",
-    TelMobile="0612345678",
-    Email="jean.dupont@example.com",
-    NumPermis="123456789",
-    LieuPermis="Paris",
-    DatePermis="2005-06-10",
-    PaimentAdhesion=True,
-    PaimentCaution=True,
-    RIBfourni="FR7630001007941234567890185",
-    numformule=1
+            Nom="Dupont",
+            Prenom="Jean",
+            DateNaissance="1985-05-15",
+            Rue="123 Rue de Paris",
+            CodePostal="75001",
+            Tel="0123456789",
+            TelMobile="0612345678",
+            Email="jean.dupont@example.com",
+            NumPermis="123456789",
+            LieuPermis="Paris",
+            DatePermis="2005-06-10",
+            PaimentAdhesion=True,
+            PaimentCaution=True,
+            RIBfourni="FR7630001007941234567890185",
+            numformule=1
 )
         Affi_adherent(13)
         print(cur.fetchall())
+        """
+        #besoin 2
+        def liste_des_tarifs(formule):
+            # formule = (« Classique » ou « Coopérative « ou « Liberté »)
+            
+        def udate_tarif  (formule):
+            # formule = (« Classique » ou « Coopérative « ou « Liberté »)
+
+        #besoin 3
+        def lst_vehicules(categorie):
+            #Liste des véhicules d’une catégorie (« S », « M » ou « L »
+
+        def aff_véhicules(numvéhicules):
+
+        def add_véhicules():
+        """
+        main = True
+        while main :
+            reponce = input("Sous-menu « Gestion des adhérents » : 1 \n" \
+            "Sous-menu « Gestion des tarifs » : 2 \n" \
+            "Sous-menu « Gestion des véhicules » : 3 \n" \
+            "Sous-menu « Requêtes » : 4 \n" \
+            "Sous-menu « Vue » : 5\n " \
+            "quitter : q \n")
+            if reponce == "q":
+                main = False
+            elif reponce =="1":
+                m1 = True
+                while m1 :
+                    reponce = input("choisiser une formule ( « Classique », « Coopérative » ou « Liberté »)\n" \
+                    "ou retourner au menu principal : q\n")
+                    if reponce == "q":
+                        m1 = False
+                    elif reponce in("Classique","Coopérative","Liberté"):
+                        lst_aderent(reponce)
+                        print(cur.fetchall())
+
+                    else :
+                        print("votre entrer comporte une erreur ")
+            elif reponce =="2":
+                pass
+            elif reponce =="3":
+                pass
+            elif reponce =="4":
+                pass
+            elif reponce =="5":
+                pass
+            else :
+                print("votre entrer comporte une erreur ")
+
+    
+    

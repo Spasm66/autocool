cf04f1a36d2f19d0697c7f047951956da28869d3
Spasm66 <loris.pfeiffer27@gmail.com>
2025-05-14 16:16:25 +0200

    update creation sql

--- script_creation.sql
+++ script_creation.sql
@@ -1,3 +1,5 @@
+--MEA BESOIN 1
+
 CREATE TABLE abonne(
     NumAbonne SERIAL,
     Nom  VARCHAR (15),
@@ -26,7 +28,7 @@ CREATE TABLE formule(
     DepotGarantie INT,
     Caution INT,
     CONSTRAINT "pk_CodeFormule" PRIMARY KEY (CodeFormule)
-    );
+);
 
 CREATE TABLE adhere(
     NumAbonne INT,
@@ -34,4 +36,20 @@ CREATE TABLE adhere(
     CONSTRAINT "pk_NumAbonne_CodeFormule" PRIMARY KEY (NumAbonne, CodeFormule),
     CONSTRAINT "fk_NumAbonne" FOREIGN KEY (NumAbonne) REFERENCES abonne(NumAbonne),
     CONSTRAINT "fk_CodeFormule" FOREIGN KEY (CodeFormule) REFERENCES formule(CodeFormule)
+);
+
+--MEA BESOIN 2
+
+CREATE TABLE trache_horaire(
+    CodeTrancheH CHAR (1) NOT NULL,
+    Duree INT,
+    CONSTRAINT "pk_CodeTrancheH" PRIMARY KEY (CodeTrancheH),
+    CONSTRAINT "chk_CodeTranche" CHECK (CodeTrancheH = 'H' OR CodeTrancheH = 'J' OR CodeTrancheH = 'S')
+);
+
+CREATE TABLE categorie_vehicule(
+    CodeCateg CHAR (1) NOT NULL,
+    LibelleCateg VARCHAR (20),
+    CONSTRAINT "pk_CodeCateg" PRIMARY KEY (CodeCateg),
+    CONSTRAINT "chk_CodeTranche" CHECK (CodeTrancheH = 'H' OR CodeTrancheH = 'J' OR CodeTrancheH = 'S')
 );

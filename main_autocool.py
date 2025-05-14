0b33de7df4881ff6727b9f9fd99942e8f7f8445f
Nicolas Andrieu <niandrieu@emi.u-bordeaux.fr>
2025-05-14 17:08:42 +0200

    ajout script_insertion.sql et validation de la 3 eme fonction

--- main_autocool.py
+++ main_autocool.py
@@ -20,7 +20,7 @@ with psycopg.connect("host="+HOST+" user="+USERNAME+" password="+PASS) as conn:
             print("Fichier creation_table exécuté avec succès.")
         except Exception as e:
             print("Une erreur est survenue (si du au fait que les table existe deja pas de probleme): "+ str(e))
-        """
+        
         # test si deja des donnes
         test_abonne = 'SELECT * FROM abonne;'
         
@@ -41,7 +41,7 @@ with psycopg.connect("host="+HOST+" user="+USERNAME+" password="+PASS) as conn:
                 print("Fichier script_insertion exécuté avec succès.")
             except Exception as e:
                 exit("Une erreur est survenue : {e}")
-            
+        """
         def lst_aderent(formule):
             # formule = (« Classique » ou « Coopérative « ou « Liberté »)
             commande = 'select NumAbonne from  formule ,adhere  where LibelleFormule =(%(id)s) and  formule.CodeFormule=adhere.CodeFormule '
@@ -74,26 +74,55 @@ with psycopg.connect("host="+HOST+" user="+USERNAME+" password="+PASS) as conn:
     Email ,
     NumPermis ,
     LieuPermis ,
-    DAtePermis ,
+    DatePermis ,
     PaimentAdhesion ,
     PaimentCaution ,
-    RIBfourni ):
-            commande = 'insert into abonne values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) '
+    RIBfourni,
+    numformule ):
+            commande_abonne = 'INSERT INTO abonne (Nom, Prenom, DateNaissance, Rue, CodePostal, Tel, TelMobile, Email, NumPermis, LieuPermis, DatePermis, PaimentAdhesion, PaimentCaution, RIBfourni)'\
+        'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+           
             try:
-                cur.execute(commande,(Nom  ,
-    Prenom ,
-    DateNaissance ,
-    Rue ,
-    CodePostal ,
-    Tel ,
-    TelMobile ,
-    Email ,
-    NumPermis ,
-    LieuPermis ,
-    DAtePermis ,
-    PaimentAdhesion ,
-    PaimentCaution ,
-    RIBfourni ))
+                cur.execute(commande_abonne, (
+            Nom, Prenom, DateNaissance, Rue, CodePostal, Tel, TelMobile, Email, NumPermis, LieuPermis, DatePermis, PaimentAdhesion, PaimentCaution, RIBfourni
+        ))
                 print("commande SQL exécuté avec succès.")
             except Exception as e:
-                exit("error when running: " + commande + " : " + str(e))
+                exit("error when running: " + commande_abonne + " : " + str(e))
+            commande = 'select max(NumAbonne) from abonne'
+            cur.execute(commande)
+            num_abonne_temp = cur.fetchall()[0]
+            num_abonne = num_abonne_temp['max']
+            print(num_abonne)
+            commande_adhere = """
+        INSERT INTO adhere (NumAbonne, CodeFormule)
+        VALUES (%s, %s);
+        """
+            try:
+                cur.execute(commande_adhere, (
+            num_abonne,numformule
+        ))
+                print("commande SQL exécuté avec succès.")
+            except Exception as e:
+                exit("error when running: " + commande_adhere + " : " + str(e))
+
+
+        Ajout_adherent(
+    Nom="Dupont",
+    Prenom="Jean",
+    DateNaissance="1985-05-15",
+    Rue="123 Rue de Paris",
+    CodePostal="75001",
+    Tel="0123456789",
+    TelMobile="0612345678",
+    Email="jean.dupont@example.com",
+    NumPermis="123456789",
+    LieuPermis="Paris",
+    DatePermis="2005-06-10",
+    PaimentAdhesion=True,
+    PaimentCaution=True,
+    RIBfourni="FR7630001007941234567890185",
+    numformule=1
+)
+        Affi_adherent(13)
+        print(cur.fetchall())

52dd70a893c2fff83f60e58e78e1774eb71abb48
Spasm66 <loris.pfeiffer27@gmail.com>
2025-05-27 16:32:34 +0200

    sous menu 2 finish

--- sousmenu2.py
+++ sousmenu2.py
@@ -39,3 +39,19 @@ def update_tarif_h(formule, CodeTrancheH, CodeCateg, new_price):
         cur.execute(command, {'price': new_price, 'formule': numformule, 'TrancheH': CodeTrancheH, 'Categ': CodeCateg})
     except Exception as e:
             exit("error when running: " + command + " : " + str(e))
+
+def update_tarif_h(formule, CodeTrancheKm, CodeCateg, new_price):
+    if formule == "Classique":
+        numformule = 1
+    elif formule == "Coopérative":
+        formule = 2
+    elif formule == "Liberté":
+        numformule = 3
+    command = 'UPDATE facturer2 SET tarifh = %(price)s ' \
+        	    'WHERE CodeTrancheH = %(TrancheKm)s ' \
+                'AND CodeCateg = %(Categ)s ' \
+                'AND CodeFormule = %(formule)s'
+    try:
+        cur.execute(command, {'price': new_price, 'formule': numformule, 'TrancheKm': CodeTrancheKm, 'Categ': CodeCateg})
+    except Exception as e:
+            exit("error when running: " + command + " : " + str(e))

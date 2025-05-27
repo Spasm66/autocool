aac9e3415af80320c91724731160f11d880be56e
Spasm66 <loris.pfeiffer27@gmail.com>
2025-05-27 16:30:34 +0200

    sous menu 2

--- sousmenu2.py
+++ sousmenu2.py
@@ -1,4 +1,4 @@
-def affi_tarif(formule):
+def affi_tarif_h(formule):
     if formule == "Classique":
         numformule = 1
     elif formule == "Coopérative":
@@ -10,3 +10,32 @@ def affi_tarif(formule):
         cur.execute(command, numformule)
     except Exception as e:
                 exit("error when running: " + command + " : " + str(e))
+
+def affi_tarif_km(formule):
+    if formule == "Classique":
+        numformule = 1
+    elif formule == "Coopérative":
+        formule = 2
+    elif formule == "Liberté":
+        numformule = 3
+    command = 'SELECT CodeTrancheKm, CodeFormule, CodeCateg, TarifKm FROM facturer2 WHERE CodeFormule = %s'
+    try:
+        cur.execute(command, numformule)
+    except Exception as e:
+                exit("error when running: " + command + " : " + str(e))
+
+def update_tarif_h(formule, CodeTrancheH, CodeCateg, new_price):
+    if formule == "Classique":
+        numformule = 1
+    elif formule == "Coopérative":
+        formule = 2
+    elif formule == "Liberté":
+        numformule = 3
+    command = 'UPDATE facturer2 SET tarifh = %(price)s ' \
+        	    'WHERE CodeTrancheH = %(TrancheH)s ' \
+                'AND CodeCateg = %(Categ)s ' \
+                'AND CodeFormule = %(formule)s'
+    try:
+        cur.execute(command, {'price': new_price, 'formule': numformule, 'TrancheH': CodeTrancheH, 'Categ': CodeCateg})
+    except Exception as e:
+            exit("error when running: " + command + " : " + str(e))
